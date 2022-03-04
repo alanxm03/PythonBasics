28854156d5a353a045b8ff64479d839cd76e0ee8
Alan Xavier Martial <52906032+alanxm03@users.noreply.github.com>
2022-03-04 16:20:12 +0530

    again

--- candies.py
+++ candies.py
@@ -1,24 +1,23 @@
 candies_count=3
-print(f"Available candies are {candies_count}")
+print(f"Available candies are {candies_count} 🍬")
 
-inp=int(input("How many Candies do you want "))
+inp=int(input("How many Candies do you want?🤔 "))
 if inp<=candies_count:
     for i in range(0,inp):
-            print("Candies")
+            print("Candies🍬")
             candies_count-=1
 else:
-    print(f"Available Candies are {candies_count} But you entered {inp}")
-    nxt=input("Continue or Exit:").lower()
+    print(f"Available Candies are {candies_count} But you entered {inp} 🤷‍♂️")
+    nxt=input("Continue or Exit🙂:").lower()
     if nxt=="continue":
         for i in range(0,candies_count):
-            print("Candies")
+            print("Candies🍬")
             candies_count-=1
 
     else:
         print("Thank You")
         exit
 if(candies_count>0):
-    print(f"Available Candies is {candies_count}")
+    print(f"Available Candies is {candies_count}🍬")
 else:
-     print("Out of Candies")
-    
+     print("Out of Candies 😬")
